42d75d1ec23864e04042a403b421e7d4e6b82dd1
Ayush Yadav <230930006+th3mech4nic@users.noreply.github.com>
2026-09-11 12:46:52 +0000

    Initialize auth middleware

--- middleware.js
+++ middleware.js
@@ -1,5 +1,18 @@
-function authMiddleware(req, res, next) {
+const { JsonWebTokenError } = require("jsonwebtoken");
 
+function authMiddleware(req, res, next) {
+    const token = req.headers.token;
+
+    const decoded = jwt.verify(token,"superkey");
+    const userId = decoded.userId;
+    if(userId){
+        req.userId;
+        next();
+    }else{
+        res.status(403).json({
+            message: "Token waas incorrect"
+        })
+    }
 }
 
 module.exports = {
